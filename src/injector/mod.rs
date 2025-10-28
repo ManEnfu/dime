73505d4275aa0bbd919d9e53ce24c9d83c617ea6
ManEnfu <manenfu@protomail.com>
2025-10-28 13:30:37 +0700

    feat: injector and state API

---
+++ src/injector/mod.rs
@@ -0,0 +1,153 @@
+//! [`Injector`] trait and common implementations.
+
+use std::any::{TypeId, type_name};
+use std::sync::Arc;
+
+use crate::erased::Erased;
+use crate::result::Result;
+
+pub mod state;
+use state::{RawWatch, Watch};
+
+/// A base trait for container to inject to and retrieve value from.
+pub trait Injector: Send + Sync {
+    /// Tells the injector that a type might be injected to it.
+    ///
+    /// Depending on the implementation, Trying to retrieve value (e.g. by calling
+    /// [`Watch::available`]) prior to calling this method for its type may panic, wait forever,
+    /// or return [`ResolutionError::NotDefined`](crate::result::ResolutionError::NotDefined).
+    /// Calling this method ensures that retrieving value of this type will wait until a value
+    /// is available.
+    fn define_by_type_id(&self, type_id: TypeId, type_name: &'static str);
+
+    /// Inject a value of a given type into the injector.
+    ///
+    /// # Panics
+    ///
+    /// The caller must ensure that the type of `value` and the type identified by `type_id`
+    /// match. Breaking this contract may cause panic or other sorts of problems.
+    /// However as, this method is safe, the implementor must ensure that calls with incorrect
+    /// arguments should *not* cause undefined behavior.
+    fn inject_by_type_id(&self, type_id: TypeId, type_name: &'static str, value: Result<Erased>);
+
+    /// Watches for values of a given type in the injector.
+    ///
+    /// # Panics
+    ///
+    /// The implementation must ensure that the type of values received by the returned
+    /// [`RawWatch`] matches with the type identified by `type_id`. Otherwise, breaking this
+    /// contract may cause panic or other sorts of problems, but should *not* cause undefined
+    /// behavior.
+    fn raw_watch_by_type_id(&self, type_id: TypeId, type_name: &'static str) -> RawWatch;
+}
+
+impl<I> Injector for Arc<I>
+where
+    I: Injector,
+{
+    fn define_by_type_id(&self, type_id: TypeId, type_name: &'static str) {
+        (**self).define_by_type_id(type_id, type_name);
+    }
+
+    fn inject_by_type_id(&self, type_id: TypeId, type_name: &'static str, value: Result<Erased>) {
+        (**self).inject_by_type_id(type_id, type_name, value);
+    }
+
+    fn raw_watch_by_type_id(&self, type_id: TypeId, type_name: &'static str) -> RawWatch {
+        (**self).raw_watch_by_type_id(type_id, type_name)
+    }
+}
+
+impl<I> Injector for Box<I>
+where
+    I: Injector,
+{
+    fn define_by_type_id(&self, type_id: TypeId, type_name: &'static str) {
+        (**self).define_by_type_id(type_id, type_name);
+    }
+
+    fn inject_by_type_id(&self, type_id: TypeId, type_name: &'static str, value: Result<Erased>) {
+        (**self).inject_by_type_id(type_id, type_name, value);
+    }
+
+    fn raw_watch_by_type_id(&self, type_id: TypeId, type_name: &'static str) -> RawWatch {
+        (**self).raw_watch_by_type_id(type_id, type_name)
+    }
+}
+
+/// Type-safe methods for [`Injector`].
+pub trait InjectorExt: Injector {
+    /// Tells the injector that a type might be injected to it.
+    ///
+    /// Depending on the implementation, Trying to retrieve value (e.g. by calling
+    /// [`Watch::available`]) prior to calling this method for its type may panic, wait forever,
+    /// or return [`ResolutionError::NotDefined`](crate::result::ResolutionError::NotDefined).
+    /// Calling this method ensures that retrieving value of this type will wait until a value
+    /// is available.
+    ///
+    /// This method is a type-safe variant of [`Injector::define_by_type_id`].
+    fn define<T>(&self)
+    where
+        T: Clone + Send + Sync + 'static,
+    {
+        self.define_by_type_id(TypeId::of::<T>(), type_name::<T>());
+    }
+
+    /// Inject a value of a given type into the injector.
+    ///
+    /// This method is a type-safe variant of [`Injector::inject_by_type_id`].
+    fn inject<T>(&self, value: Result<T>)
+    where
+        T: Clone + Send + Sync + 'static,
+    {
+        self.inject_by_type_id(TypeId::of::<T>(), type_name::<T>(), value.map(Erased::new));
+    }
+
+    /// Watches for values of a given type in the injector.
+    ///
+    /// This method is a type-safe variant of [`Injector::raw_watch_by_type_id`].
+    fn watch<T>(&self) -> Watch<T>
+    where
+        T: Clone + Send + Sync + 'static,
+    {
+        let raw = self.raw_watch_by_type_id(TypeId::of::<T>(), type_name::<T>());
+
+        Watch::from_raw(raw)
+    }
+}
+
+impl<I> InjectorExt for I where I: ?Sized + Injector {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dyn_compatible() {
+        pub struct FakeInjector;
+
+        impl Injector for FakeInjector {
+            fn define_by_type_id(&self, _type_id: TypeId, _type_name: &'static str) {}
+
+            fn inject_by_type_id(
+                &self,
+                _type_id: TypeId,
+                _type_name: &'static str,
+                _value: Result<Erased>,
+            ) {
+            }
+
+            fn raw_watch_by_type_id(&self, type_id: TypeId, type_name: &'static str) -> RawWatch {
+                state::RawState::new_undefined(type_id, type_name).watch()
+            }
+        }
+
+        fn check_dyn_compatible(injector: &dyn Injector) {
+            injector.define::<i32>();
+            injector.inject(Ok("hello"));
+            let _ = injector.watch::<bool>();
+        }
+
+        check_dyn_compatible(&FakeInjector);
+    }
+}
---
+++ src/injector/state.rs
@@ -0,0 +1,305 @@
+//! Type value states.
+
+use std::any::TypeId;
+use std::marker::PhantomData;
+
+use tokio::sync::watch;
+
+use crate::{
+    erased::Erased,
+    result::{ResolutionError, Result},
+};
+
+#[derive(Clone, Debug, Default)]
+enum Inner {
+    #[default]
+    Undefined,
+    Pending,
+    Ready(Result<Erased>),
+}
+
+impl Inner {
+    const fn is_defined(&self) -> bool {
+        !matches!(self, Self::Undefined)
+    }
+
+    const fn is_available(&self) -> bool {
+        matches!(self, Self::Ready(_))
+    }
+
+    fn define(&mut self) -> bool {
+        if matches!(self, Self::Undefined) {
+            *self = Self::Pending;
+            true
+        } else {
+            false
+        }
+    }
+}
+
+/// A state of a given type in [`Injector`](crate::injector::Injector).
+#[derive(Debug, Clone)]
+pub struct RawState {
+    inner: watch::Sender<Inner>,
+    type_id: TypeId,
+    type_name: &'static str,
+}
+
+/// Watches for type-erased values of a given type in [`Injector`](crate::injector::Injector).
+///
+/// This is a *raw* version of the watch, which works with [`Erased`] values.
+/// To work with values of concrete types, consider using [`Watch`].
+#[derive(Debug, Clone)]
+pub struct RawWatch {
+    inner: watch::Receiver<Inner>,
+    type_id: TypeId,
+    type_name: &'static str,
+}
+
+/// Watches for values of a given type in [`Injector`](crate::injector::Injector).
+#[derive(Debug, Clone)]
+pub struct Watch<T> {
+    raw: RawWatch,
+    _marker: PhantomData<T>,
+}
+
+impl RawState {
+    fn new_inner(inner: Inner, type_id: TypeId, type_name: &'static str) -> Self {
+        let (tx, _) = watch::channel(inner);
+
+        Self {
+            inner: tx,
+            type_id,
+            type_name,
+        }
+    }
+
+    /// Creates a new state-watch pair that has not been defined.
+    pub fn new_undefined(type_id: TypeId, type_name: &'static str) -> Self {
+        Self::new_inner(Inner::Undefined, type_id, type_name)
+    }
+
+    /// Creates a new state-watch pair that has been defined and is ready to receive values.
+    pub fn new_defined(type_id: TypeId, type_name: &'static str) -> Self {
+        Self::new_inner(Inner::Pending, type_id, type_name)
+    }
+
+    /// Creates a new state-watch pair with a value already available.
+    ///
+    /// # Panics
+    ///
+    /// See [`Injector::inject_by_type_id`](crate::injector::Injector::inject_by_type_id).
+    pub fn new_value(type_id: TypeId, type_name: &'static str, value: Result<Erased>) -> Self {
+        Self::new_inner(Inner::Ready(value), type_id, type_name)
+    }
+
+    /// Tells the state a type might be injected to it.
+    pub fn define(&self) {
+        self.inner.send_if_modified(Inner::define);
+    }
+
+    /// Injects a value into the state.
+    ///
+    /// # Panics
+    ///
+    /// See [`Injector::inject_by_type_id`](crate::injector::Injector::inject_by_type_id).
+    pub fn inject(&self, value: Result<Erased>) {
+        self.inner.send_replace(Inner::Ready(value));
+    }
+
+    /// Returns true if the type has been defined for the state.
+    pub fn is_defined(&self) -> bool {
+        self.inner.borrow().is_defined()
+    }
+
+    /// Returns true if the type has been available.
+    pub fn is_available(&self) -> bool {
+        self.inner.borrow().is_available()
+    }
+
+    /// Returns a watch for this state.
+    pub fn watch(&self) -> RawWatch {
+        let rx = self.inner.subscribe();
+        RawWatch::new(rx, self.type_id, self.type_name)
+    }
+}
+
+impl RawWatch {
+    const fn new(inner: watch::Receiver<Inner>, type_id: TypeId, type_name: &'static str) -> Self {
+        Self {
+            inner,
+            type_id,
+            type_name,
+        }
+    }
+
+    /// Waits until a value (or an error that occurs while trying to create such value) of type
+    /// associated with this watch is available.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if the type is not yet defined in the
+    /// injector, an error occured during the construction of the value, or the injector has been
+    /// dropped.
+    pub async fn available(&mut self) -> Result<Erased> {
+        self.inner
+            .wait_for(|state| !matches!(state, Inner::Pending))
+            .await
+            .map_err(ResolutionError::other)
+            .and_then(|state| match &*state {
+                Inner::Undefined => Err(ResolutionError::NotDefined(self.type_id, self.type_name)),
+                Inner::Pending => unreachable!(),
+                Inner::Ready(result) => result.clone(),
+            })
+    }
+
+    /// Waits until the type associated with this watch is defined.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if the injector has been dropped.
+    pub async fn defined(&mut self) -> Result<()> {
+        self.inner
+            .wait_for(|state| !matches!(state, Inner::Undefined))
+            .await
+            .map_err(ResolutionError::other)?;
+
+        Ok(())
+    }
+
+    /// Waits until the type associated with this watch is defined and a value is available.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if an error occured during the
+    /// construction of the value, or the injector has been dropped.
+    pub async fn defined_and_available(&mut self) -> Result<Erased> {
+        self.inner
+            .wait_for(|state| matches!(state, Inner::Ready(_)))
+            .await
+            .map_err(ResolutionError::other)
+            .and_then(|state| match &*state {
+                Inner::Ready(result) => result.clone(),
+                _ => unreachable!(),
+            })
+    }
+
+    /// Waits until the state of the type associated with this watch is changed.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if the injector has been dropped.
+    pub async fn changed(&mut self) -> Result<()> {
+        self.inner.changed().await.map_err(ResolutionError::other)
+    }
+
+    /// Returns true if the state of the type associated with this watch has been changed.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if the injector has been dropped.
+    pub fn has_changed(&self) -> Result<bool> {
+        self.inner.has_changed().map_err(ResolutionError::other)
+    }
+
+    /// Returns true if the type has been defined for the injector.
+    pub fn is_defined(&self) -> bool {
+        self.inner.borrow().is_defined()
+    }
+
+    /// Returns true if the type has been available.
+    pub fn is_available(&self) -> bool {
+        self.inner.borrow().is_available()
+    }
+}
+
+impl<T> Watch<T>
+where
+    T: Clone + Send + Sync + 'static,
+{
+    /// Creates a watch from [`RawWatch`].
+    ///
+    /// # Panics
+    ///
+    /// Panic may occur if `T` and the underlying type of the values observed by [`RawWatch`] does
+    /// not match.
+    pub(crate) fn from_raw(raw: RawWatch) -> Self {
+        debug_assert_eq!(TypeId::of::<T>(), raw.type_id);
+
+        Self {
+            raw,
+            _marker: PhantomData,
+        }
+    }
+
+    /// Waits until a value (or an error that occurs while trying to create such value) of type
+    /// associated with this watch is available.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if the type is not yet defined in the
+    /// injector, an error occured during the construction of the value, or the injector has been
+    /// dropped.
+    pub async fn available(&mut self) -> Result<T> {
+        self.raw.available().await.map(|value| {
+            #[expect(clippy::missing_panics_doc)]
+            value.downcast::<T>().unwrap()
+        })
+    }
+
+    /// Waits until the type associated with this watch is defined.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if the injector has been dropped.
+    #[inline]
+    pub async fn defined(&mut self) -> Result<()> {
+        self.raw.defined().await
+    }
+
+    /// Waits until the type associated with this watch is defined and a value is available.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if an error occured during the
+    /// construction of the value, or the injector has been dropped.
+    #[inline]
+    pub async fn defined_and_available(&mut self) -> Result<T> {
+        self.raw.defined_and_available().await.map(|value| {
+            #[expect(clippy::missing_panics_doc)]
+            value.downcast::<T>().unwrap()
+        })
+    }
+
+    /// Waits until the state of the type associated with this watch is changed.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if the injector has been dropped.
+    #[inline]
+    pub async fn changed(&mut self) -> Result<()> {
+        self.raw.changed().await
+    }
+
+    /// Returns true if the state of the type associated with this watch has been changed.
+    ///
+    /// # Errors
+    ///
+    /// This method returns returns [`ResolutionError`] if the injector has been dropped.
+    #[inline]
+    pub fn has_changed(&mut self) -> Result<bool> {
+        self.raw.has_changed()
+    }
+
+    /// Returns true if the type has been defined for the injector.
+    #[inline]
+    pub fn is_defined(&self) -> bool {
+        self.raw.is_defined()
+    }
+
+    /// Returns true if the type has been available.
+    #[inline]
+    pub fn is_available(&self) -> bool {
+        self.raw.is_available()
+    }
+}
