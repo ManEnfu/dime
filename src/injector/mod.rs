62e3d334c609a8746574f9a68584378ad41300ac
ManEnfu <manenfu@protomail.com>
2025-10-30 12:45:20 +0700

    feat: update `InjectorTask` and add `InjectorTaskObject`

--- src/injector/mod.rs
+++ src/injector/mod.rs
@@ -123,7 +123,9 @@ pub trait InjectorExt: Injector {
 impl<I> InjectorExt for I where I: ?Sized + Injector {}
 
 /// A task operating around an injector.
-pub trait InjectorTask {
+pub trait InjectorTask<I> {
+    type Future: Future<Output = Result<()>> + Send;
+
     /// Run the task with the given injector.
     ///
     /// # Errors
@@ -131,22 +133,82 @@ pub trait InjectorTask {
     /// The semantics of the error returned by this method may vary between implementations, but
     /// in the most common case, this method will return an error if the underlying task encounters
     /// an unexpected error, panicks, or is otherwise unable to continue in any way.
-    fn run(
-        self,
-        injector: &(dyn Injector + Send + Sync),
-    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send>>;
+    fn run(self, injector: &I) -> Self::Future;
 }
 
-impl<F, Fut> InjectorTask for F
+impl<I, F, Fut> InjectorTask<I> for F
 where
-    F: FnOnce(&(dyn Injector + Send + Sync)) -> Fut,
+    F: FnOnce(&I) -> Fut,
     Fut: Future<Output = Result<()>> + Send + 'static,
 {
-    fn run(
-        self,
-        injector: &(dyn Injector + Send + Sync),
-    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send>> {
-        Box::pin(self(injector))
+    type Future = Fut;
+
+    #[inline]
+    fn run(self, injector: &I) -> Self::Future {
+        self(injector)
+    }
+}
+
+/// A dispatchable [`InjectorTask`] trait object.
+///
+/// Use this instead of `Box<dyn InjectorTask>` to dynamically dispatch [`InjectorTask::run`].
+pub struct InjectorTaskObject<I> {
+    // We don't store `InjectorTask` trait object here, as `run` method is non-dispatchable,
+    // using boxed `FnOnce` allow us to consume the boxed value when calling `run`.
+    #[allow(clippy::type_complexity)]
+    boxed: Box<dyn FnOnce(&I) -> Pin<Box<dyn Future<Output = Result<()>> + Send>> + Send>,
+    concrete_type: &'static str,
+}
+
+impl<I> InjectorTaskObject<I> {
+    /// Creates a new `BoxedInjectorTask` from a concrete task.
+    pub fn new<T>(task: T) -> Self
+    where
+        T: InjectorTask<I> + Send + 'static,
+        T::Future: Send + 'static,
+    {
+        let wrapped_fn = |injector: &I| {
+            Box::pin(task.run(injector)) as Pin<Box<dyn Future<Output = Result<()>> + Send>>
+        };
+
+        Self {
+            boxed: Box::new(wrapped_fn),
+            concrete_type: type_name::<T>(),
+        }
+    }
+
+    /// Creates a new `BoxedInjectorTask` from a concrete task that returns future that implements.
+    /// `Into<Pin<Box<dyn Future<Output = Result<()>> + Send>>>`.
+    ///
+    /// This is useful to avoid extra allocation and indirection.
+    pub fn from_boxed_future<T>(task: T) -> Self
+    where
+        T: InjectorTask<I> + Send + 'static,
+        T::Future: Into<Pin<Box<dyn Future<Output = Result<()>> + Send>>>,
+    {
+        let wrapped_fn = |injector: &I| task.run(injector).into();
+
+        Self {
+            boxed: Box::new(wrapped_fn),
+            concrete_type: type_name::<T>(),
+        }
+    }
+}
+
+impl<I> std::fmt::Debug for InjectorTaskObject<I> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BoxedInjectorTask")
+            .field("concrete_type", &self.concrete_type)
+            .finish_non_exhaustive()
+    }
+}
+
+impl<I> InjectorTask<I> for InjectorTaskObject<I> {
+    type Future = Pin<Box<dyn Future<Output = Result<()>> + Send>>;
+
+    #[inline]
+    fn run(self, injector: &I) -> Self::Future {
+        self.boxed.run(injector)
     }
 }
 
