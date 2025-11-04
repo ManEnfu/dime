649a18fc85f263a9f0d3cbae10edaf90d44ce75e
ManEnfu <manenfu@protomail.com>
2025-11-04 15:56:54 +0700

    cleanup: move `DynClone` trait to `erased` module

--- src/erased.rs
+++ src/erased.rs
@@ -2,7 +2,24 @@
 
 use std::any::Any;
 
-use crate::DynClone;
+/// Clones a trait object into a [`Box`].
+///
+/// As [`Clone`] requires its implementor to be [`Sized`], it is not
+/// dyn-compatible. This trait allows safe cloning on DSTs and trait objects
+/// at the cost of heap allocation.
+pub trait DynClone: Any + Send + Sync {
+    /// Clones `self` and wraps it inside a [`Box`].
+    fn dyn_clone(&self) -> Box<dyn DynClone>;
+}
+
+impl<T> DynClone for T
+where
+    T: Any + Clone + Send + Sync,
+{
+    fn dyn_clone(&self) -> Box<dyn DynClone> {
+        Box::new(self.clone())
+    }
+}
 
 /// [`Erased`] is a container for value of an arbitrary type, as long as it
 /// implements [`Clone`], [`Send`], and [`Sync`] and is `'static`.
--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,13 @@
 #![warn(clippy::nursery)]
 #![allow(clippy::must_use_candidate)]
 
-pub use dyn_clone::DynClone;
-
 #[macro_use]
 pub(crate) mod macros;
 
-mod dyn_clone;
 pub mod erased;
-
 pub mod result;
-
 pub mod runtime;
 
-pub mod injector;
-
 pub mod component;
-
 pub mod container;
+pub mod injector;
